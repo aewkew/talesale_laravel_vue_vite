93858aff1c1c9e00afbf70de85068bc0aeaff79e
aewkew <geeg2012@hotmail.com>
2022-10-09 16:52:50 +0700

    Update UserProfile.vue
    Co-Authored-By: espadaulqiorrano4 <54341654+espadaulqiorrano4@users.noreply.github.com>

--- resources/js/pages/UserProfile.vue
+++ resources/js/pages/UserProfile.vue
@@ -1,6 +1,42 @@
 <template >
-    <div>
+    <div class="container">
         <UserCard> </UserCard>
+
+        <div class="sale">User list</div>
+
+        <div class="tableContrainer">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id Customer</th>
+                        <th scope="col">Customer name</th>
+                        <th scope="col">Personel name</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Details</th>
+
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr v-for="item in data" v-bind:key="item.productId">
+                        <th scope="row"></th>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.description }}</td>
+                        <td>{{ item.price }}</td>
+              
+                        <td>
+                            <button
+                                class="but-co btn "
+                                type="button"
+                            >
+                                <i class="bi bi-plus"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div> 
+
     </div>
 </template>
 <script>
