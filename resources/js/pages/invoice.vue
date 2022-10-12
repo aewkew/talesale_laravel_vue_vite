c16f6fd7c32b8cfaac150ecaa9c252d1f8906b36
aewkew <geeg2012@hotmail.com>
2022-10-12 15:20:47 +0700

    Sawaddekrap

--- resources/js/pages/invoice.vue
+++ resources/js/pages/invoice.vue
@@ -1,15 +1,21 @@
-<template >
+<template>
     <div class="container">
-        I'm invoice
-        
+        <div class="sale">Invoice</div>
+
+        <div class="row">
+            <div class="col">
+                <div class="invoice-title">
+                    <h2>Invoice</h2>
+                    <h3 class="pull-right">Order # 12345</h3>
+                </div>
+            </div>
+            <hr>
+        </div>
     </div>
 </template>
 <script>
 export default {
     name: "Invoice",
-    
-}
+};
 </script>
-<style >
-    
-</style>
+<style></style>
