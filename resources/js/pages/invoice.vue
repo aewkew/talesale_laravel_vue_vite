cd892bfed6c2bdb5132e7e0f2dbdb546cd1f1d73
aewkew <geeg2012@hotmail.com>
2022-10-14 00:08:01 +0700

    invoice dat

--- resources/js/pages/invoice.vue
+++ resources/js/pages/invoice.vue
@@ -24,40 +24,60 @@
         </div>
 
         <!-- Invoice   -->
-        
-    <div class="invoice">
-        <div class="invoice-data">
-        <div class="row">
-            <div class="col">
-                <div class="logo-invoice">
-                    <img src="https://seeklogo.com/images/B/Burger_King-logo-67A54F414B-seeklogo.com.png"/>
+
+        <div class="invoice">
+            <div class="invoice-data">
+                <div class="row">
+                    <div class="col">
+                        <div class="logo-invoice">
+                            <img
+                                src="https://seeklogo.com/images/B/Burger_King-logo-67A54F414B-seeklogo.com.png"
+                            />
+                        </div>
+                        <div class="row">
+                            <div class="company">
+                                Talesale cartridge Company
+                            </div>
+                            <div class="company-address">
+                                2033/02 Rung Arun, <br />
+                                HangDong, Chaing Mai 50230<br />
+                                ,Thailand <br />
+                                084-61061646 ,Talesale_12@hotmille.com<br />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="invoice-number">
+                            <div>INVOICE</div>
+                            <div>#INV-002408</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="row">
-                    <div class="company">Talesale cartridge Company </div>
-                    <div class="company-address"> 2033/02 Rung Arun, <br>
-                                                  HangDong, <br>
-                                                  Chaing Mai 50230, <br>
-                                                  Thailand <br>
+                    <div class="bill">
+                        <div class="row"> 
+                        <div class="col">
+                            <div class="bill-name">BILL TO</div>
+                            <div class="bill-data">
+                                Tiger kung <br />
+                                Jungti kung Comp <br />
+                                Sansai Chaingmai 50210 <br />
+                                06-46166415 , Jungti_ger_kung@hitmill.com
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="data-invoice">
+                                Invoice Date :  10/14/2022    <br/>
+                                Terms :  Due on Receipt   <br/>
+                                Due Date :   10/05/2016 
+                            </div>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="invoice-number">
-                <div> INVOICE </div> 
-                <div> #INV-002408 </div>
-                </div>
-
-            </div>
         </div>
-        <div class="row">
-            <div class="col"></div>
-            <div class="col"></div>
-        </div>
-     </div>
-
-     </div>
-    
-
     </div>
 </template>
 <script>
